9edcb6a7d5049c1730814bc168f5f780c3e324fe
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2024-02-13 12:53:13 +0530

    add file

--- CRUD-in-Nodejs/index.js
+++ CRUD-in-Nodejs/index.js
@@ -1,16 +1,20 @@
-const express = require("express");
+const express = require("express")
 const products = require("./model");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDocs = YAML.load("./api.yaml");
 require("./config");
 const app =express();
 const port = process.env.PORT|5000;
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 // POST API
 app.use(express.json());
 app.post("/create",async (req,resp) => {
     let data = new products(req.body);
     let result = await data.save();
-    resp.send(data);
-    console.log(data);
+    resp.send(result);
+    console.log(result);
 })
 
 // GET API
@@ -28,16 +32,20 @@ app.delete("/delete/:_id",async(req,resp) => {
 })
 
 // UPDATE API
-app.put("/update/:_id",(req,resp) => {
-    let data = products.updateOne(
-        req.params,
-        {
-            $set:req.body
-        }        
-    );
-    resp.send(data);
-    console.log(data);
-})
+app.put("/update/:_id", async (req, resp) => {
+    try {
+        const data = await products.updateOne(
+            { _id: req.params._id }, // corrected to use req.params._id for the ID
+            { $set: req.body }
+        );
+        console.log("All Good");
+        resp.send(data);
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+        resp.status(500).send("Internal Server Error");
+    }
+});
 
 app.listen(port,() => {
     console.log("app is listenng the port number 5000..!")
